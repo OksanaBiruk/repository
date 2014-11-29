ae29567f2db04ebab94c2ef16ac09218b48101d3
OksanaBiruk <bioks07@gmail.com>
2014-11-29 23:04:08 +0200

    Test for Form registration using Cucumber. Not valid data. Scenario Outline

---
+++ FormRegister/features/step_definitions/steps.rb
@@ -0,0 +1,25 @@
+require 'selenium-webdriver'
+require 'rspec'
+
+browser = Selenium::WebDriver.for :firefox
+
+Given(/^I am on the Front Page$/) do
+  browser.get 'http://demo.redmine.org'
+end
+
+When(/^I click on "([^"]*)" link$/) do |link_text|
+  browser.find_element(link_text:link_text).click
+end
+
+And(/^I fill up "([^"]*)" text field with (.*)$/) do |text_field_id, value|
+  browser.find_element(id:text_field_id).clear
+  browser.find_element(id:text_field_id).send_keys value
+end
+
+And(/^I click on "([^"]*)" button$/) do |button_name|
+  browser.find_element(name:button_name).click
+end
+
+Then(/^Warning (.*) is appeared$/) do |message|
+  expect(browser.find_element(id:'errorExplanation').text).to include message
+end
